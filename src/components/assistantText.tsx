dabb734c5ef8d999cd7a9b2a23a0a226060e479c
tegnike <o3nike.teg.14@gmail.com>
2023-05-12 11:02:10 +0200

    フォーム上のVコメントの文字を大きく変更

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -4,7 +4,7 @@ export const AssistantText = ({ message }: { message: string }) => {
       <div className="mx-auto max-w-4xl w-full p-16">
         <div className="bg-white rounded-8">
           <div className="px-24 py-16">
-            <div className="line-clamp-4 text-secondary typography-16 font-M_PLUS_2 font-bold">
+            <div className="line-clamp-4 text-secondary typography-20 font-M_PLUS_2 font-bold">
               {message.replace(/\[([a-zA-Z]*?)\]/g, "")}
             </div>
           </div>
