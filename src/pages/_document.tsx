48f201ecb08794e71231eece438d8748f03a719b
o3nike.teg.14@gmail.com <o3nike.teg.14@gmail.com>
2023-06-16 19:47:59 +0200

    静的ファイルのパス不具合修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <script src="https://unpkg.com/core-js-bundle@3.6.1/minified.js"></script>
-        <script src="/Core/live2dcubismcore.js"></script>
+        <script src={`${buildUrl("/Core/live2dcubismcore.js")}`}></script>
       </Head>
       <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
         <Main />
